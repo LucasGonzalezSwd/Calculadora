c5df599ac017327f47cad4f425be179158339166
LucasGonzalezSwd <lucas.ing.ind@gmail.com>
2023-09-11 20:05:57 -0300

    remove bg

--- Calculadora/src/app/page.tsx
+++ Calculadora/src/app/page.tsx
@@ -2,7 +2,7 @@ import App from './Components/App'
 import './globals.css'
 export default function Calculadora() {
   return (
-    <div className='flex justify-center mt-[3rem] bg-red-500'>
+    <div className='flex justify-center mt-[3rem]'>
        <App/>
     </div>
   )
