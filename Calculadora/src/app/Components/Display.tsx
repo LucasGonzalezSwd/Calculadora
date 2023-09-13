f117c2946a78555a2479cb32033983642e2d70fd
LucasGonzalezSwd <lucas.ing.ind@gmail.com>
2023-09-13 17:05:51 -0300

    font size on display when 295px

--- Calculadora/src/app/Components/Display.tsx
+++ Calculadora/src/app/Components/Display.tsx
@@ -5,9 +5,9 @@ import { DisplayProps } from '../../types'
 
 export default function Display ({value} : DisplayProps) {
     return (
-      <div className='bg-[#8ca992] w-[80%] h-[4rem] items-center flex justify-end text-4xl rounded-xl shadow-lg shadow-gray-900/40 drop-shadow-2xl  overflow-hidden  border-[0.001rem] border-[#16171a] mt-[0.3rem]' data-testid="display-component  ">
+      <div className='bg-[#8ca992] w-[80%] h-[4rem] items-center flex justify-end text-4xl rounded-xl shadow-lg shadow-gray-900/40 drop-shadow-2xl  overflow-hidden  border-[0.001rem] border-[#16171a] mt-[0.3rem] ' data-testid="display-component  ">
         <div>
-          <h1 className='letraD pr-[0.5rem] '>{value}</h1></div>
+          <h1 className='letraD pr-[0.5rem] max-[295px]:text-5xl'>{value}</h1></div>
       </div>
     )
 }
