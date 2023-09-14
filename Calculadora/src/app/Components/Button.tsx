28f9e6fd1c9c0e178483a8af90934ad3ff97b4cc
LucasGonzalezSwd <lucas.ing.ind@gmail.com>
2023-09-14 16:07:08 -0300

    press button

--- Calculadora/src/app/Components/Button.tsx
+++ Calculadora/src/app/Components/Button.tsx
@@ -6,17 +6,17 @@ export default function Boton ({clickHandle, name} : ButtonProps) {
 
   const handleClick = () => clickHandle(name)
 
-  let buttonStyle = 'bg-[#EBEBEB] w-[4rem] h-[3.5rem] rounded-xl border-b-[#a0a6a1] border-b-4 font-bold text-3xl shadow-sm shadow-black/100 max-[319px]:w-[3rem] max-[319px]:h-[2.5rem] max-[319px]:text-2xl max-[900px]:w-[3.5rem] max-[900px]:h-[3rem] text-black';
+  let buttonStyle = 'bg-[#EBEBEB] w-[4rem] h-[3.5rem] rounded-xl border-b-[#a0a6a1] border-b-4 font-bold text-3xl shadow-sm shadow-black/100 max-[319px]:w-[3rem] max-[319px]:h-[2.5rem] max-[319px]:text-2xl max-[900px]:w-[3.5rem] max-[900px]:h-[3rem] text-black active:translate-y-1';
 
   if (name === '=') {
-    buttonStyle = 'bg-[#F7A54E]  w-[4rem] h-[3.5rem] rounded-xl border-b-[#D4852C] border-b-4 font-bold text-3xl shadow-sm shadow-black/100 max-[319px]:w-[3rem] max-[319px]:h-[2.5rem] max-[319px]:text-2xl max-[900px]:w-[3.5rem] max-[900px]:h-[3rem] text-black';
+    buttonStyle = 'bg-[#F7A54E]  w-[4rem] h-[3.5rem] rounded-xl border-b-[#D4852C] border-b-4 font-bold text-3xl shadow-sm shadow-black/100 max-[319px]:w-[3rem] max-[319px]:h-[2.5rem] max-[319px]:text-2xl max-[900px]:w-[3.5rem] max-[900px]:h-[3rem] text-black active:translate-y-1';
   }
 
   if (name === 'AC') {
-    buttonStyle = 'bg-[#c42020]   w-[4rem] h-[3.5rem] rounded-xl border-b-[#9d1919] border-b-4 font-bold text-3xl shadow-sm shadow-black/100 max-[319px]:w-[3rem] max-[319px]:h-[2.5rem] max-[319px]:text-2xl max-[900px]:w-[3.5rem] max-[900px]:h-[3rem] text-black';
+    buttonStyle = 'bg-[#c42020]   w-[4rem] h-[3.5rem] rounded-xl border-b-[#9d1919] border-b-4 font-bold text-3xl shadow-sm shadow-black/100 max-[319px]:w-[3rem] max-[319px]:h-[2.5rem] max-[319px]:text-2xl max-[900px]:w-[3.5rem] max-[900px]:h-[3rem] text-black active:translate-y-1';
   }
   if (name === '+') {
-    buttonStyle = 'bg-[#EBEBEB] w-[4rem] h-[8.6rem] rounded-xl border-b-[#a0a6a1] border-b-4 font-bold text-3xl absolute shadow-sm shadow-black/100 max-[319px]:w-[3rem] max-[319px]:h-[6.6rem] max-[319px]:text-2xl max-[900px]:w-[3.5rem] max-[900px]:h-[7.57rem] text-black';
+    buttonStyle = 'bg-[#EBEBEB] w-[4rem] h-[8.6rem] rounded-xl border-b-[#a0a6a1] border-b-4 font-bold text-3xl absolute shadow-sm shadow-black/100 max-[319px]:w-[3rem] max-[319px]:h-[6.6rem] max-[319px]:text-2xl max-[900px]:w-[3.5rem] max-[900px]:h-[7.57rem] text-black active:translate-y-1';
   }
 
       return (
--- Calculadora/src/app/page.tsx
+++ Calculadora/src/app/page.tsx
@@ -2,7 +2,7 @@ import App from './Components/App'
 import './globals.css'
 export default function Calculadora() {
   return (
-    <div className='flex justify-center h-screen w-screen  background'>
+    <div className='flex justify-center h-full w-screen  background'>
        <App/>
     </div>
   )
